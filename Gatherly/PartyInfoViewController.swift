7ba27facff36d2d1dad0ba049aad0c5d352b1abc
raymww <raymond.m.wang@gmail.com>
2025-03-12 18:46:08 -0500

    added RSVP Selector

--- Gatherly/PartyInfoViewController.swift
+++ Gatherly/PartyInfoViewController.swift
@@ -7,17 +7,22 @@
 
 import UIKit
 import FirebaseFirestore
+import FirebaseAuth
 
 
 class PartyInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var party: Party?
     var invitees: [User] = []
+    var rsvps : [String : String] = [:]
     @IBOutlet weak var partyName: UILabel!
     @IBOutlet weak var partyID: UILabel!
     @IBOutlet weak var partyDescription: UILabel!
     @IBOutlet weak var partyDate: UILabel!
     @IBOutlet weak var partyTime: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var rsvpSelector: UISegmentedControl!
+    
+    
     let cellIdentifier = "InviteeCell"
     
     override func viewDidLoad() {
@@ -33,12 +38,104 @@ class PartyInfoViewController: UIViewController, UITableViewDelegate, UITableVie
         tableView.delegate = self
         tableView.dataSource = self
         fetchInvitees()
+        initRsvp()
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         updateDarkMode(darkMode: darkMode, to: view)
+    }
+    
+    @IBAction func rsvpChanged(_ sender: Any) {
+        switch rsvpSelector.selectedSegmentIndex {
+            case 0 :
+                // "Going"
+                setRsvp(new: "Going")
+            case 1 :
+                // "Not Going
+                setRsvp(new: "Not Going")
+            case 2 :
+                // "Undecided"
+                setRsvp(new: "Undecided")
+            default:
+                print("Problem with RSVP Selector")
+        }
+        
+    }
+    
+    func initRsvp() {
+        guard let uid = Auth.auth().currentUser?.uid else {
+            showAlert(on: self, title: "Error", message: "No user logged in.")
+            return
+        }
+        
+        let partyId = party!.partyId
+        
+        Firestore.firestore().collection("parties").document(partyId).getDocument { partyDoc, error in
+            if let error = error {
+                print("Error fetching party \(partyId): \(error.localizedDescription)")
+                return
+            }
+            if let partyData = partyDoc?.data(),
+               let hostUid = partyData["hostUid"] as? String {
+                if (hostUid == uid) {
+                    self.rsvpSelector.isHidden = true
+                }
+            }
+
+        }
+        
+        let userRef = Firestore.firestore().collection("users").document(uid)
+        userRef.getDocument { document, error in
+            if let error = error {
+                showAlert(on: self, title: "Error", message: "Failed to fetch user data: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = document?.data(),
+                let rsvpStatus = data["rsvps"] as? [String : String] else {
+                showAlert(on: self, title: "Error", message: "Invalid user data format.")
+                return
+            }
+            self.rsvps = rsvpStatus
+            
+            switch rsvpStatus[self.party!.partyId] {
+            case "Going":
+                self.rsvpSelector.selectedSegmentIndex = 0
+            case "Not Going":
+                self.rsvpSelector.selectedSegmentIndex = 1
+            case "Undecided":
+                self.rsvpSelector.selectedSegmentIndex = 2
+            default:
+                self.rsvpSelector.selectedSegmentIndex = 2
+            }
+            
+        }
+    }
+    
+    func setRsvp(new : String){
+        guard let uid = Auth.auth().currentUser?.uid else {
+            showAlert(on: self, title: "Error", message: "No user logged in.")
+            return
+        }
+        
+        self.rsvps[self.party!.partyId] = new
+        
+
+        let db = Firestore.firestore()
+        
+        db.collection("users").document(uid).updateData([
+            "rsvps" : self.rsvps
+        ]) { error in
+            if let error = error {
+                showAlert(on: self, title: "Error", message: "Failed to update RSVP: \(error.localizedDescription)")
+            } else {
+                print("Updated RSVP to \(new)")
+            }
+        }
+        fetchInvitees()
+        self.tableView.reloadData()
     }
     
     func fetchInvitees() {
